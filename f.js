09793b6125004e2435960a83f178510a223e8f88
diogocerutti <diogo.cerutti@unochapeco.edu.br>
2021-09-20 14:56:22 -0300

    Arquivo de css e js

---
+++ f.js
@@ -0,0 +1,40 @@
+window.ready = function(){
+
+}
+
+function re(){
+
+    var r = document.getElementById("marca");
+    var i;
+    for(i=0;i<89;i++){
+        r=i
+        console.log(r);
+        
+    }
+    }
+
+
+    <table border="1" id="tabela">
+    <tr>
+        <td>Valor</td>
+        <td>Marca</td>
+        <td>Modelo</td>
+        <td>Ano Modelo</td>
+        <td>Combustivel</td>
+        <td>Codigo Fipe</td>
+        <td>Mes Referencia</td>
+        <td>Tipo Veiculo</td>
+        <td>Sigla Combustivel</td>
+    </tr>
+    <tr>
+        <th id="res_valor"></th>
+        <th id="res_marca"></th>
+        <th id="res_modelo"></th>
+        <th id="res_anomodelo"></th>
+        <th id="res_combustivel"></th>
+        <th id="res_codigofipe"></th>
+        <th id="res_mesreferencia"></th>
+        <th id="res_tipoveiculo"></th>
+        <th id="res_siglacombustivel"></th>
+    </tr>
+</table>
